cc3fc8c34df9a84a7b30dada7244eb4259e34858
Adrian F. Cobena <afcobena.energy@gmail.com>
2023-10-24 22:49:50 +0200

    deploy test 4

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_URL}/auth`,
+    baseURL: `${process.env.REACT_APP_SERVER_URL}`,
     /* withCredentials: true, */
 })
 
