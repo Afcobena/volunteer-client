8fc020be2987d4d000a15bc10eccaaea4bae6fc4
Adrian F. Cobena <afcobena.energy@gmail.com>
2023-12-12 05:51:56 +0100

    navbar structure modification

--- src/components/navigation/navbar/Navbar.jsx
+++ src/components/navigation/navbar/Navbar.jsx
@@ -1,54 +1,79 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {useContext} from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
-import {AuthContext} from "../../../context/auth.context"
-
+import { AuthContext } from "../../../context/auth.context";
 
 function Navbar() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const {isUserActive, authenticatedUser, isUserAdmin, user} = useContext(AuthContext)
+  const { isUserActive, authenticatedUser, isUserAdmin, user } =
+    useContext(AuthContext);
 
   const handleLogout = () => {
     // destruir el token con removeItem de LocalStorage
-    localStorage.removeItem("authToken")
-    authenticatedUser()
-    navigate("/")
-  }
+    localStorage.removeItem("authToken");
+    authenticatedUser();
+    navigate("/");
+  };
 
   if (isUserActive === true) {
     return (
-      <div className='nav'>
-        <Link className='links' to={"/"}>HOME</Link>
-        <Link className='links' to={"/proposals"}>PROPOSALS</Link>
+      <div className="nav">
+        <div className="nav--image">
+          <img src="" alt="" />
+        </div>
 
-        <Link className='links' to={"/profile"}>PROFILE</Link>
+        <div className="nav--links">
+          <Link className="links" to={"/"}>
+            HOME
+          </Link>
+          <Link className="links" to={"/proposals"}>
+            PROPOSALS
+          </Link>
+
+          <Link className="links" to={"/profile"}>
+            PROFILE
+          </Link>
+        </div>
 
         <div className="signup-button">
-        <button className='sign-btn' onClick={handleLogout}>Logout</button>
+          <button className="sign-btn" onClick={handleLogout}>
+            Logout
+          </button>
         </div>
-
-
       </div>
-    )
+    );
   } else {
     return (
-      <div>
-
-        <div className='nav'>
-          <Link className='links' to={"/"}>HOME</Link>
-          <Link className='links' to={"/proposals"}>PROPOSALS</Link>
-          <Link className='links' to={"/signup"}>SIGN UP</Link>
-          <Link className='links' to={"/login"}>LOGIN</Link>
+      <div className="nav">
+        <div className="nav--image">
+          <img src="" alt="" />
         </div>
-      
-      </div>
-    )
-  }
 
-  
+        <div className="nav--links">
+          <Link className="links" to={"/"}>
+            HOME
+          </Link>
+          <Link className="links" to={"/proposals"}>
+            PROPOSALS
+          </Link>
+          <Link className="links" to={"/signup"}>
+            SIGN UP
+          </Link>
+          <Link className="links" to={"/login"}>
+            LOGIN
+          </Link>
+        </div>
+
+        <div className="signup-button">
+          <button className="sign-btn" onClick={handleLogout}>
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
